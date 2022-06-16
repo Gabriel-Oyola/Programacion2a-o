8b837d3ce371e4d227cd5a0f62cb5a8dc9eaba83
Gabriel-Oyola <90019281+Gabriel-Oyola@users.noreply.github.com>
2022-06-16 04:54:21 -0300

    Add files via upload

---
+++ WindowsFomsProducto1/FormsProductos.Designer.cs
@@ -0,0 +1,339 @@
+﻿namespace WindowsFomsProducto1
+{
+    partial class FormsProductos
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btCargar = new System.Windows.Forms.Button();
+            this.tabProductos = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.txtStock = new System.Windows.Forms.TextBox();
+            this.lbStock = new System.Windows.Forms.Label();
+            this.txtDesc = new System.Windows.Forms.TextBox();
+            this.lbDesc = new System.Windows.Forms.Label();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.lblStockActual = new System.Windows.Forms.Label();
+            this.lbDescMov = new System.Windows.Forms.Label();
+            this.lbCodMov = new System.Windows.Forms.Label();
+            this.rbEgreso = new System.Windows.Forms.RadioButton();
+            this.rbIngreso = new System.Windows.Forms.RadioButton();
+            this.btCargaM = new System.Windows.Forms.Button();
+            this.txtBuscar = new System.Windows.Forms.TextBox();
+            this.lbCantidad = new System.Windows.Forms.Label();
+            this.dgvProductos = new System.Windows.Forms.DataGridView();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lblBuscar = new System.Windows.Forms.Label();
+            this.btBuscar = new System.Windows.Forms.Button();
+            this.txtCant = new System.Windows.Forms.TextBox();
+            this.btEliminar = new System.Windows.Forms.Button();
+            this.tabProductos.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.tabPage2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvProductos)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btCargar
+            // 
+            this.btCargar.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btCargar.Location = new System.Drawing.Point(381, 172);
+            this.btCargar.Name = "btCargar";
+            this.btCargar.Size = new System.Drawing.Size(170, 38);
+            this.btCargar.TabIndex = 3;
+            this.btCargar.Text = "Cargar";
+            this.btCargar.UseVisualStyleBackColor = true;
+            this.btCargar.Click += new System.EventHandler(this.btCargar_Click);
+            // 
+            // tabProductos
+            // 
+            this.tabProductos.Controls.Add(this.tabPage1);
+            this.tabProductos.Controls.Add(this.tabPage2);
+            this.tabProductos.Location = new System.Drawing.Point(12, 12);
+            this.tabProductos.Name = "tabProductos";
+            this.tabProductos.SelectedIndex = 0;
+            this.tabProductos.Size = new System.Drawing.Size(776, 278);
+            this.tabProductos.TabIndex = 4;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.txtStock);
+            this.tabPage1.Controls.Add(this.lbStock);
+            this.tabPage1.Controls.Add(this.btCargar);
+            this.tabPage1.Controls.Add(this.txtDesc);
+            this.tabPage1.Controls.Add(this.lbDesc);
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(768, 252);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "Carga de productos";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // txtStock
+            // 
+            this.txtStock.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtStock.Location = new System.Drawing.Point(10, 178);
+            this.txtStock.Name = "txtStock";
+            this.txtStock.Size = new System.Drawing.Size(289, 26);
+            this.txtStock.TabIndex = 4;
+            // 
+            // lbStock
+            // 
+            this.lbStock.AutoSize = true;
+            this.lbStock.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbStock.Location = new System.Drawing.Point(10, 151);
+            this.lbStock.Name = "lbStock";
+            this.lbStock.Size = new System.Drawing.Size(85, 24);
+            this.lbStock.TabIndex = 3;
+            this.lbStock.Text = "STOCK:";
+            // 
+            // txtDesc
+            // 
+            this.txtDesc.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtDesc.Location = new System.Drawing.Point(6, 114);
+            this.txtDesc.Name = "txtDesc";
+            this.txtDesc.Size = new System.Drawing.Size(545, 26);
+            this.txtDesc.TabIndex = 2;
+            // 
+            // lbDesc
+            // 
+            this.lbDesc.AutoSize = true;
+            this.lbDesc.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbDesc.Location = new System.Drawing.Point(6, 87);
+            this.lbDesc.Name = "lbDesc";
+            this.lbDesc.Size = new System.Drawing.Size(153, 24);
+            this.lbDesc.TabIndex = 1;
+            this.lbDesc.Text = "DESCRIPCION:";
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Controls.Add(this.btEliminar);
+            this.tabPage2.Controls.Add(this.txtCant);
+            this.tabPage2.Controls.Add(this.btBuscar);
+            this.tabPage2.Controls.Add(this.lblBuscar);
+            this.tabPage2.Controls.Add(this.label1);
+            this.tabPage2.Controls.Add(this.lblStockActual);
+            this.tabPage2.Controls.Add(this.lbDescMov);
+            this.tabPage2.Controls.Add(this.lbCodMov);
+            this.tabPage2.Controls.Add(this.rbEgreso);
+            this.tabPage2.Controls.Add(this.rbIngreso);
+            this.tabPage2.Controls.Add(this.btCargaM);
+            this.tabPage2.Controls.Add(this.txtBuscar);
+            this.tabPage2.Controls.Add(this.lbCantidad);
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(768, 252);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "Movimiento de productos";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // lblStockActual
+            // 
+            this.lblStockActual.AutoSize = true;
+            this.lblStockActual.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblStockActual.Location = new System.Drawing.Point(19, 120);
+            this.lblStockActual.Name = "lblStockActual";
+            this.lblStockActual.Size = new System.Drawing.Size(60, 20);
+            this.lblStockActual.TabIndex = 7;
+            this.lblStockActual.Text = "Stock:";
+            // 
+            // lbDescMov
+            // 
+            this.lbDescMov.AutoSize = true;
+            this.lbDescMov.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbDescMov.Location = new System.Drawing.Point(18, 77);
+            this.lbDescMov.Name = "lbDescMov";
+            this.lbDescMov.Size = new System.Drawing.Size(108, 20);
+            this.lbDescMov.TabIndex = 6;
+            this.lbDescMov.Text = "Descripcion:";
+            // 
+            // lbCodMov
+            // 
+            this.lbCodMov.AutoSize = true;
+            this.lbCodMov.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbCodMov.Location = new System.Drawing.Point(18, 38);
+            this.lbCodMov.Name = "lbCodMov";
+            this.lbCodMov.Size = new System.Drawing.Size(70, 20);
+            this.lbCodMov.TabIndex = 5;
+            this.lbCodMov.Text = "Codigo:";
+            // 
+            // rbEgreso
+            // 
+            this.rbEgreso.AutoSize = true;
+            this.rbEgreso.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.rbEgreso.Location = new System.Drawing.Point(196, 181);
+            this.rbEgreso.Name = "rbEgreso";
+            this.rbEgreso.Size = new System.Drawing.Size(80, 22);
+            this.rbEgreso.TabIndex = 4;
+            this.rbEgreso.TabStop = true;
+            this.rbEgreso.Text = "Egreso";
+            this.rbEgreso.UseVisualStyleBackColor = true;
+            // 
+            // rbIngreso
+            // 
+            this.rbIngreso.AutoSize = true;
+            this.rbIngreso.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.rbIngreso.Location = new System.Drawing.Point(196, 153);
+            this.rbIngreso.Name = "rbIngreso";
+            this.rbIngreso.Size = new System.Drawing.Size(82, 22);
+            this.rbIngreso.TabIndex = 3;
+            this.rbIngreso.TabStop = true;
+            this.rbIngreso.Text = "Ingreso";
+            this.rbIngreso.UseVisualStyleBackColor = true;
+            // 
+            // btCargaM
+            // 
+            this.btCargaM.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btCargaM.Location = new System.Drawing.Point(284, 153);
+            this.btCargaM.Name = "btCargaM";
+            this.btCargaM.Size = new System.Drawing.Size(125, 54);
+            this.btCargaM.TabIndex = 2;
+            this.btCargaM.Text = "Cargar";
+            this.btCargaM.UseVisualStyleBackColor = true;
+            this.btCargaM.Click += new System.EventHandler(this.btCargaM_Click);
+            // 
+            // txtBuscar
+            // 
+            this.txtBuscar.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtBuscar.Location = new System.Drawing.Point(413, 32);
+            this.txtBuscar.Name = "txtBuscar";
+            this.txtBuscar.Size = new System.Drawing.Size(100, 26);
+            this.txtBuscar.TabIndex = 1;
+            // 
+            // lbCantidad
+            // 
+            this.lbCantidad.AutoSize = true;
+            this.lbCantidad.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbCantidad.Location = new System.Drawing.Point(19, 181);
+            this.lbCantidad.Name = "lbCantidad";
+            this.lbCantidad.Size = new System.Drawing.Size(86, 20);
+            this.lbCantidad.TabIndex = 0;
+            this.lbCantidad.Text = "Cantidad:";
+            // 
+            // dgvProductos
+            // 
+            this.dgvProductos.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvProductos.Location = new System.Drawing.Point(12, 307);
+            this.dgvProductos.Name = "dgvProductos";
+            this.dgvProductos.Size = new System.Drawing.Size(772, 200);
+            this.dgvProductos.TabIndex = 5;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(341, 116);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 20);
+            this.label1.TabIndex = 8;
+            // 
+            // lblBuscar
+            // 
+            this.lblBuscar.AutoSize = true;
+            this.lblBuscar.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblBuscar.Location = new System.Drawing.Point(409, 9);
+            this.lblBuscar.Name = "lblBuscar";
+            this.lblBuscar.Size = new System.Drawing.Size(70, 20);
+            this.lblBuscar.TabIndex = 9;
+            this.lblBuscar.Text = "Buscar:";
+            // 
+            // btBuscar
+            // 
+            this.btBuscar.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btBuscar.Location = new System.Drawing.Point(519, 32);
+            this.btBuscar.Name = "btBuscar";
+            this.btBuscar.Size = new System.Drawing.Size(125, 26);
+            this.btBuscar.TabIndex = 10;
+            this.btBuscar.Text = "Buscar";
+            this.btBuscar.UseVisualStyleBackColor = true;
+            this.btBuscar.Click += new System.EventHandler(this.btBuscar_Click);
+            // 
+            // txtCant
+            // 
+            this.txtCant.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtCant.Location = new System.Drawing.Point(23, 204);
+            this.txtCant.Name = "txtCant";
+            this.txtCant.Size = new System.Drawing.Size(100, 26);
+            this.txtCant.TabIndex = 11;
+            // 
+            // btEliminar
+            // 
+            this.btEliminar.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btEliminar.Location = new System.Drawing.Point(284, 110);
+            this.btEliminar.Name = "btEliminar";
+            this.btEliminar.Size = new System.Drawing.Size(125, 26);
+            this.btEliminar.TabIndex = 12;
+            this.btEliminar.Text = "Eliminar";
+            this.btEliminar.UseVisualStyleBackColor = true;
+            this.btEliminar.Click += new System.EventHandler(this.btEliminar_Click);
+            // 
+            // FormsProductos
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.ClientSize = new System.Drawing.Size(800, 541);
+            this.Controls.Add(this.dgvProductos);
+            this.Controls.Add(this.tabProductos);
+            this.Name = "FormsProductos";
+            this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide;
+            this.Text = "FormsProductos";
+            this.tabProductos.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage1.PerformLayout();
+            this.tabPage2.ResumeLayout(false);
+            this.tabPage2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvProductos)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button btCargar;
+        private System.Windows.Forms.TabControl tabProductos;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.TextBox txtStock;
+        private System.Windows.Forms.Label lbStock;
+        private System.Windows.Forms.TextBox txtDesc;
+        private System.Windows.Forms.Label lbDesc;
+        private System.Windows.Forms.Label lbDescMov;
+        private System.Windows.Forms.Label lbCodMov;
+        private System.Windows.Forms.RadioButton rbEgreso;
+        private System.Windows.Forms.RadioButton rbIngreso;
+        private System.Windows.Forms.Button btCargaM;
+        private System.Windows.Forms.TextBox txtBuscar;
+        private System.Windows.Forms.Label lbCantidad;
+        private System.Windows.Forms.DataGridView dgvProductos;
+        private System.Windows.Forms.Label lblStockActual;
+        private System.Windows.Forms.Label lblBuscar;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btBuscar;
+        private System.Windows.Forms.TextBox txtCant;
+        private System.Windows.Forms.Button btEliminar;
+    }
+}
---
+++ WindowsFomsProducto1/FormsProductos.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Entidades;
+
+
+
+namespace WindowsFomsProducto1
+{
+    public partial class FormsProductos : Form
+    {
+        Producto producto;
+        public ListaProducto lista { get; set; } = new ListaProducto();
+        public FormsProductos()
+        {
+            InitializeComponent(); 
+            dgvProductos.DataSource = lista.ListaProductos;
+        }
+
+        private void btCargar_Click(object sender, EventArgs e)
+        {
+           
+
+            producto = new Producto(txtDesc.Text, int.Parse(txtStock.Text));
+
+
+
+            lista.NuevoProducto(producto);
+
+            
+
+
+        }
+
+        private void btBuscar_Click(object sender, EventArgs e)
+        {
+            producto = lista.BuscarProducto(int.Parse(txtBuscar.Text));
+
+            if (producto.pcodigo > 0)
+            {
+                lbCodMov.Text= "Codigo "+ producto.pcodigo.ToString();
+                lbDescMov.Text = "Descripcion: "+ producto.PDescripcion;
+                lblStockActual.Text="Stock: "+producto.PStock.ToString();
+            }
+            else
+            {
+                MessageBox.Show("El producto no existe");
+            }
+        }
+
+        private void btCargaM_Click(object sender, EventArgs e)
+        {
+            if (rbEgreso.Checked == true)
+            {
+                producto.Salida(int.Parse(txtCant.Text));
+                lista.NuevoProducto(producto);
+            }
+
+            if (rbIngreso.Checked == true)
+            {
+                producto.Ingreso(int.Parse(txtCant.Text));
+                lista.NuevoProducto(producto);
+            }
+        }
+
+        private void btEliminar_Click(object sender, EventArgs e)
+        {
+            if (lista.DeleteProducto(producto))
+            {
+                Limpiar();
+            }
+            else
+            {
+                MessageBox.Show("El producto codigo " + producto.pcodigo + " no se pudo borrar");
+                Limpiar();
+            }
+        }
+
+        private void Limpiar()
+        {
+            lbCodMov.Text = "Codigo: ";
+            lbDescMov.Text = "Descripcion: ";
+            lblStockActual.Text = "Stock: "; 
+        }
+    }
+}
